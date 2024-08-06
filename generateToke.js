17e9c47d635c0411ad39a1f5ee099877aadbb2b7
theFury07 <51827036+Rahul-Adapa@users.noreply.github.com>
2024-08-06 22:35:28 +0530

    Add files via upload

---
+++ generateToke.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res) => {
+    const { clientId, source } = req.query;
+    const secretKey = process.env.SECRET_KEY;
+
+    if (!clientId || !source) {
+        return res.status(400).json({ error: 'Missing required fields: clientId and source' });
+    }
+
+    try {
+        const token = jwt.sign({ clientId, source }, secretKey, { expiresIn: '1h' });
+        return res.status(200).json({ token });
+    } catch (error) {
+        console.error('Error generating JWT:', error);
+        return res.status(500).json({ error: 'Error generating JWT' });
+    }
+};
